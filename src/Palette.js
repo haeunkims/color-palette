6cf03b8117cf9fe920a9fce402c78b0c098be7ce
Haeun Kim <gracekim.an@gmail.com>
2021-11-23 16:38:32 +0900

    adding colorHelper to generate colors

--- src/Palette.js
+++ src/Palette.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import { generatePalette } from "./colorHelper";
 import "./Palette.css";
 class Palette extends Component {
   render() {
