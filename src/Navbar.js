796d32fbc8a7072849f587a9b73f3ee283766fc9
Haeun Kim <gracekim.an@gmail.com>
2021-12-01 14:51:26 +0900

    Adding selector

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 import Slider, { Range } from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -18,7 +30,7 @@ class Navbar extends Component {
               defaultValue={level}
               min={100}
               max={900}
-              onAfterChange={this.props.changeLevel}
+              onAfterChange={changeLevel}
               step={100}
               trackStyle={{ backgroundColor: "transparent" }}
               handleStyle={{
@@ -33,6 +45,13 @@ class Navbar extends Component {
               railStyle={{ heigh: 8 }}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
+          </Select>
         </div>
       </header>
     );
