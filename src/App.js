745ccf8c9ef8e8f198f91d0610d1121abd11f543
Haeun Kim <gracekim.an@gmail.com>
2021-12-07 16:44:40 +0900

    adding colorseed palette routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React, { Component } from "react";
-import { Routes, Route } from "react-router";
+import { Switch, Route } from "react-router";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelper";
-// import { Switch } from "@mui/material";
 class App extends Component {
   findPalette(id) {
     return seedColors.find(function (palette) {
-      return palette.id == id;
+      return palette.id === id;
     });
   }
   render() {
     return (
-      <Routes>
-        <Route path="/welcome" element={<h1>MainPage </h1>} />
-        {/*need help change to JSX */}
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette Lists</h1>} />
         <Route
           exact
           path="/palette/:id"
           render={(routeProps) => (
             <Palette
               palette={generatePalette(
-                this.findPalette[routeProps.match.params.id]
+                this.findPalette(routeProps.match.params.id)
               )}
             />
           )}
         />
-      </Routes>
+      </Switch>
+      //   <Route
+      //     exact
+      //     path="/palette/:id"
+      //     render={(routeProps) => (
+      //       <Palette
+      //         palette={generatePalette(
+      //           this.findPalette(routeProps.match.params.id)
+      //         )}
+      //       />
+      //     )}
+      //   />
+      // </Switch>
       // <div>
       //   <Palette palette={generatePalette(seedColors[4])} />
       // </div>
